350e7f903df6d8a85a3974cb91c5b0750b3fb25e
Bruno piastre <brunopiastre@pop-os.localdomain>
2023-09-08 18:56:50 -0300

    Fixed class cell

--- game/cell.py
+++ game/cell.py
@@ -12,3 +12,11 @@ class Cell:
 
     def add_letter(self, letter:Tile):
         self.letter = letter
+
+    def calculate_value(self):
+        if self.letter is None:
+            return 0
+        if self.multiplier_type == 'letter':
+            return self.letter.value * self.multiplier
+        else:
+            return self.letter.value
