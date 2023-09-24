282ed394ddf0da2e3f22735698bdb798ee78bbd6
Bruno piastre <brunopiastre@pop-os.localdomain>
2023-09-24 18:48:53 -0300

    dictionary new funtcions

--- game/dictionary.py
+++ game/dictionary.py
@@ -4,22 +4,17 @@ class Dictionary:
         self.dictionary = dictionary
 
 
-with open('diccionario.txt', 'r') as archivo:
+with open('diccionario.txt') as archivo:
     contenido = archivo.read()
     
 print(contenido)
 
 
-
-
-       'COMO HAGO PARA QUE LEA LAS PALABRAS DEL ARCHIVO DICCIONARIO'
-
-    def diccionario(diccionario.txt):
-        try:
-            with open(diccionario.txt, 'r') as archivo:
-                palabras = archivo.read().splitlines()
-            return palabras
-        except FileNotFoundError:
-            print("El archivo no fue encontrado.")
-            return []
-    
+def diccionario(contenido):
+    try:
+        with open('diccionario.txt', 'r') as archivo:
+            palabras = archivo.read().splitlines()
+        return palabras
+    except FileNotFoundError:
+        print("El archivo no fue encontrado.")
+        return []
