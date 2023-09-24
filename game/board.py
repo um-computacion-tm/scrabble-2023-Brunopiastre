917e9a7d8beba4f1ef9a7e84fa643d74d3497367
Bruno piastre <brunopiastre@pop-os.localdomain>
2023-09-24 18:45:35 -0300

    board new funtcions

--- game/board.py
+++ game/board.py
@@ -1,5 +1,4 @@
-from game.cell import Cell
-from game.tile import Tile
+from game.cell import Cell  
 
 class Board:
     def __init__(self):
@@ -8,15 +7,18 @@ class Board:
             for _ in range(15)
         ]
 
+
+
     def calculate_word_value(self,word):
         aux = 0
         suma = 0
         list = []
+    
         
         for aux in range(len(word)):          
             
             if word[aux].active == False:
-                    word[aux].multiplier = 1
+                    word[aux].multiplier = 1          #Por que multiplier y active no tiene color??
             
             if word[aux].multiplier_type == 'letter':
                     suma += (word[aux].letter.value * word[aux].multiplier)
@@ -28,5 +30,55 @@ class Board:
                 suma = suma * word[_].multiplier
         return suma
     
+
+
+
+
+
+
+
+
+    def validate_word (self, word, file):
+        wordletter = ""
+        for i in word:
+            wordletter += i.letter.letter
+        wordletter = wordletter.lower()
+        with open(file, 'r') as archivo:
+            palabras = archivo.read().splitlines()
+            if wordletter == palabras:
+                return True
+            else:
+                return False
+    
+
+#Nuevo metodo, ver que hace
+
+
+    def validate_word_inside_board(self, word, location, orientation):
+
+        #WORK IN PROGRESS
+
+        position_x = location[0]
+
+        position_y = location[1]
+
+        len_word = len(word)
+
+        if orientation == "H":
+
+            if position_x + len_word > 15:
+
+                return False
+
+            else:
+
+                return True
+
+        else:
+
+            pass
+
+
+
 
     
