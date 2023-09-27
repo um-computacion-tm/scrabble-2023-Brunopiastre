de38ed002fb55184ce171a3382f2e70e8b8efa7b
Bruno piastre <brunopiastre@pop-os.localdomain>
2023-09-27 17:43:20 -0300

    new Dictionary Class & Test

--- game/dictionary.py
+++ game/dictionary.py
@@ -1,20 +1,3 @@
-class Dictionary:
-     
-    def __init__(self, dictionary = "diccionario.txt" ):
-        self.dictionary = dictionary
-
-
-with open('diccionario.txt') as archivo:
-    contenido = archivo.read()
-    
-print(contenido)
-
-
-def diccionario(contenido):
-    try:
-        with open('diccionario.txt', 'r') as archivo:
-            palabras = archivo.read().splitlines()
-        return palabras
-    except FileNotFoundError:
-        print("El archivo no fue encontrado.")
-        return []
+class Dictionary: 
+    def __init__(self, file_path= 'diccionario.txt'):
+        self.file_path = file_path
--- tests/test_dictionary.py
+++ tests/test_dictionary.py
@@ -6,41 +6,27 @@ from game.tile import Tile
 
 
 class TestDictionary(unittest.TestCase):
-
-    def test_validate_word_true(self):
-      # file = dictionary_file_path #que hace? 
-       diccionario = Dictionary()
-       board = Board()
-       word = [
+    
+    def test_dictionary_true(self):        
+        dictionary = Dictionary()
+        file = dictionary.file_path
+        board = Board()
+        word = [
             Cell(letter=Tile('C', 1)),
             Cell(letter=Tile('A', 1)),
             Cell(letter=Tile('S', 2)),
-            Cell(letter=Tile('A', 1)),
-        ]
-    
-       file = diccionario.dictionary 
-       result = board.validate_word(word, file)
-       self.assertEqual(result, True)
-
-
-    def test_validate_word_false(self):
-        #file = dictionary_file_path #que hace? 
-       diccionario = Dictionary()
-       board = Board()
-       word = [
+            Cell(letter=Tile('A', 1)),]
+        self.assertEqual(board.check_word(word,file),True)    
+    def test_dictionary_false(self):        
+        dictionary = Dictionary()
+        file = dictionary.file_path
+        board = Board()
+        word = [
             Cell(letter=Tile('C', 1)),
-            Cell(letter=Tile('A', 1)),
+            Cell(letter=Tile('C', 1)),
             Cell(letter=Tile('S', 2)),
-            Cell(letter=Tile('A', 1)),
-        ]
-    
-       file = diccionario.dictionary 
-       result = board.validate_word(word, file)
-       self.assertEqual(result, False)
-
-
-
-
+            Cell(letter=Tile('A', 1)),]
+        self.assertEqual(board.check_word(word,file),False)
         
 if __name__ == '__main__':
     unittest.main()
